40114fa1c6b88f0f8335650ff5ff8b73bbfccb33
Dipgohil7227 <ahird1639@gmail.com>
2025-08-14 10:23:39 +0530

    new component for the list

---
+++ 08minicontext/src/Mycomponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+function Mycomponent() {
+    const[foods, setfoods] = useState(["apple","banana", "orange"])
+
+    function handleAddFood ()
+    {
+        const newFood = document.getElementById("foodinput").value;
+        document.getElementById("foodinput").value ="";
+
+        setfoods(f => [...f,newFood])
+    }
+    function handleremoveFoods(index){
+        setfoods(foods.filter((_,i)=> i !== index))
+        console.log(foods)
+    }
+  return (
+    <div>
+        <h2>List of Food</h2>
+        <ul>
+            {foods.map((food,index)=><li key={index} onClick={()=> handleremoveFoods(index)}>{food}</li>)}
+        </ul>
+        <input type="text" id="foodinput" placeholder='Enter the Food Name '/>
+        <button onClick={handleAddFood} >Add food    </button>
+    </div>
+  )
+}
+
+export default Mycomponent
